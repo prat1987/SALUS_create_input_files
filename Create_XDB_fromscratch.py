44a5222d651dc0e2320d7625c4faa174120982a0
prateek sharma <prateeksharma@prateeks-MacBook-Pro.local>
2022-10-20 13:59:38 -0400

    Updated code

--- Create_XDB_fromscratch.py
+++ Create_XDB_fromscratch.py
@@ -6,7 +6,8 @@ from streamlit_tags import st_tags
 import xml
 import pandas as pd
 import numpy as np
-from write_xdb import write_xdb,check_exists_error,average_bd
+from create_xdb import create_xdb
+from misc_func import check_exists_error
 st.write("""
 # Create Experimental file (*xdb.xml) for SALUS
 """)
@@ -34,11 +35,7 @@ st.sidebar.markdown('## Step 1: Provide names for input files')
 sdb_file= st.sidebar.text_input('Soil file name','CONUS.sdb.xml')
 #@st.cache(hash_funcs={ET: lambda _: None})
 #@st.cache(hash_funcs={xml.etree.ElementTree.parse: lambda _: None},suppress_st_warning=True)
-def read_soil(sdb_file):
-    st.write("Reading in soil data")
-    tree = xml.etree.ElementTree.parse(sdb_file)
-    root = tree.getroot()
-    return root,tree
+
 def add_values_in_dict(sample_dict, key, list_of_values):
     ''' Append multiple values to a key in 
         the given dictionary '''
@@ -422,9 +419,9 @@ else:
             get_data().append({"crop_name":crop_name, "crop_mode": crop_mode, "speciesID":speciesID,\
                    "cultivar": cultivar,"plant_depth":plant_depth,"yearp":yearp,"ppop":ppop ,"rowspc":rowspc,\
                     "pdoy":pdoy,"irrFlag":irrFlag,"irr_method":irr_method,"dsoil":dsoil,"thetaC":thetaC,"endPt":endPt,\
-                    "fert_amt_frac_list":fert_amt_frac_list,"fdoy_list":fdoy_list,"yearf_list":yearf_list,\
+                    "Num_fert_event":Num_fert_event,"fert_amt_frac_list":fert_amt_frac_list,"fdoy_list":fdoy_list,"yearf_list":yearf_list,\
                      "fert_type_list":fert_type_list,"fert_depth_list":fert_depth_list,"fert_code_list":fert_code_list,"Nfert":N_rate_list,\
-                    "tdoy_list":tdoy_list,"yeart_list":yeart_list,"tillage_tool_list":tillage_tool_list,"tillage_depth_list":tillage_depth_list,\
+                    "Num_till_event":Num_till_event,"tdoy_list":tdoy_list,"yeart_list":yeart_list,"tillage_tool_list":tillage_tool_list,"tillage_depth_list":tillage_depth_list,\
                        "yearh":yearh, "hdoy":hdoy,"hcom":hcom,"hpc":hpc,"knock_down":knock_down,"hbpc":hbpc})
         else:
             st.write('First check all the error msg for the data input')
@@ -439,8 +436,7 @@ else:
          # Read in the soil file data.
 
     
-    # Keep track of the experiment numbers.
-    expID = 0
+    
     wx_id = "39.7333N_89.9333W"
     #load = st.checkbox('Load soil data')
     #if load:
@@ -449,96 +445,29 @@ else:
     if st.button('Create file'):
         st.write('Inside button')
         Main_datframe=pd.DataFrame(get_data())
-        root,tree=read_soil(sdb_file)
-        with open(xdb_file, 'w') as out:
-            out.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
-            out.write("<XDB>\n")
+        #root,tree=read_soil(sdb_file)
+        create_xdb(
+        xdb_file,
+        sdb_file,
+        wdb_file,
+        cdb_file,
+        wx_id,
+        cultivar,
+        soilID_list,
+        N_rate_list,
+        SALUS_start,
+        SALUS_end,
+        Main_datframe,
+        start_doy=start_doy,
+        Irrigation=Irrigation,
+        Planting=Planting,
+        split_fertapp=split_fertapp,
+         Tillage=Tillage,
+         Harvest=Harvest)
 
-            # Loop through the SoilIDs.
-            for i,soil_id in enumerate(soilID_list):
-                print("-Writing soil", soil_id)
-                # Convert soilID to a string
-                soil_id = str(soil_id)
 
-                # Get soil parameters for initial conditions.
-                # Calculate the average Bulk Density for layers 0-30cm and 30cm-bottom.
-                avg_bd_top, avg_bd_bottom, depth_top, depth_bottom, max_depth = average_bd(soil_id, root, 30)
-
-                # Loop through the N rates.
-                for N_rate in N_rate_list:
-                    print("---Writing N rate", N_rate)
-
-                    expID += 1
-
-                    # Experiment title to distinguish the differences in each experiment.
-                    title = (
-                            str(N_rate)
-                            + "|" + str(soil_id)
-                    )
-                    out.write("  <Experiment ExpID=\"" + str(expID) + "\" Title=\"" + title
-                            + "\" NYrs=\"" + str(nyrs) + "\" SYear=\"" + str(SALUS_start)
-                            + "\" SDOY=\"" + str(start_doy) + "\" ISwWat=\"Y\" ISwNit=\"Y\" ISwPho=\"N\" Frop=\"1\" Soilfp=\"" + sdb_file
-                            + "\" SoilID=\"" + str(soil_id)
-                            + "\" Weatherfp=\"" + wdb_file + "\" StationID=\"" + wx_id
-                            + "\" Cropfp=\"" + cdb_file
-                            + "\" NRrepSq=\"0\" MeEvp=\"R\" MeInf=\"R\" kResOrg=\"3.914E-5\" kSloOrg=\"0.00013699\" >\n")
-                    n_kg = 30
-                    n1_kg = n_kg * 0.4  # Top 30 cm get 40% of the N (value from Bruno a while ago)
-                    n2_kg = n_kg * 0.6
-                    n1_ppm = '%.2f' % (n1_kg / (depth_top * avg_bd_top * 10000) * 1000)
-                    out.write("    <Mgt_InitialCond Year=\"" + str(SALUS_start) + "\" DOY=\"" + str(start_doy) + "\" >\n")
-                    out.write("      <Layer DLayrI=\"30\" INinorg=\"" + str(n1_ppm) + "\" />\n")
-                    # Always write out an additional layer below 30cm, even for LS otherwise the initalized N will not work.
-                    if pd.isnull(avg_bd_bottom):
-                        max_depth = 200
-                        n2_ppm = 0
-                    else:
-                        n2_ppm = '%.2f' % (n2_kg / (depth_bottom * avg_bd_bottom * 10000) * 1000)
-                    out.write("      <Layer DLayrI=\"" + str(max_depth) + "\" INinorg=\"" + str(n2_ppm) + "\" />\n")
-                    out.write("    </Mgt_InitialCond>\n")
-                    # Component Parameters.
-                    out.write("    <Rotation_Components>\n")
-                    comp=0
-                    for i,yearp in enumerate(Main_datframe['yearp']):
-                        #st.write(i,yearp)
-                        comp += 1
-                        out.write("      <Component OrderNum=\"" + str(comp) + "\" Title=\"" + Main_datframe['crop_name'][i]
-                            + "\" IPltI=\"R\" IIrrI=\"" +  Main_datframe['irrFlag'][i] + "\" IferI=\"R\" IResI=\"R\" ITilI=\"R\" IHarI=\"R\" IEnvI=\"N\">\n")
-                        
-                        if Irrigation:
-                            out.write("        <Mgt_Irrigation_Auto DSoil=\"" + str(Main_datframe['dsoil'][i]) + "\" IAMe=\"" + Main_datframe['irr_method'][i]
-                                + "\" ThetaC=\"" + str(Main_datframe['thetaC'][i]) + "\" IEPt=\"" + str(Main_datframe['endPt'][i]) + "\" />\n")
-                        if Planting:
-                            #st.write(Main_datframe['crop_name'][i])
-                            out.write("        <Mgt_Planting CropMod=\"" + Main_datframe['crop_mode'][i] + "\" SpeciesID=\"" + Main_datframe['speciesID'][i] + "\" CultivarID=\"" +  Main_datframe['cultivar'][i]
-                                + "\" Year=\"" + str(yearp) + "\" DOY=\"" + str(Main_datframe['pdoy'][i])
-                                + "\" Ppop=\"" + str(Main_datframe['ppop'][i]) + "\" Ppoe=\"" + str(Main_datframe['ppop'][i])
-                                + "\" PlMe=\"S\" PlDs=\"R\" RowSpc=\"" + str(rowspc)
-                                + "\" SDepth=\"" + str(plant_depth) + "\" />\n")
-                        if split_fertapp:
-                            for j in range(Num_fert_event):
-                                fert_amt_kg = "{0:.2f}".format((float(N_rate) * Main_datframe['fert_amt_frac_list'][i][j]))
-                                out.write("        <Mgt_Fertilizer_App Year=\"" + str(Main_datframe['yearf_list'][i][j]) + "\" DOY=\"" + str(Main_datframe['fdoy_list'][i][j])
-                                + "\" IFType=\"" + Main_datframe['fert_type_list'][i][j] + "\" FerCode=\"" + Main_datframe['fert_code_list'][i][j]
-                                + "\" FInP=\"100\" DFert=\"" + Main_datframe['fert_depth_list'][i][j] + "\" ACrbFer=\"0\" ANFer=\"" + fert_amt_kg
-                                + "\" APFer=\"0\" AKFer=\"0\" ACFer=\"0\" AOFer=\"0\" FerDecRt=\"0\" VolN=\"0\" VolNRate=\"0\" />\n")
-
-                        if Tillage:
-                            Tillgae_comp_list=[len(tdoy_list),len(tillage_tool_list),len(tillage_depth_list)]
-                            for j in range(Num_till_event):
-                                out.write("        <Mgt_Tillage_App Year=\"" + str(Main_datframe['yeart_list'][i][j]) + "\" DOY=\"" + str(Main_datframe['tdoy_list'][i][j])
-                                        + "\" TImpl=\"" + str(Main_datframe['tillage_tool_list'][i][j]) + "\" TDep=\"" + str(Main_datframe['tillage_depth_list'][i][j]) + "\" />\n")
-                        
-                        if Harvest:
-                            out.write("        <Mgt_Harvest_App Year=\"" + str(Main_datframe['yearh'][i]) + "\" DOY=\"" + str(Main_datframe['hdoy'][i])
-                            + "\" HCom=\"" + Main_datframe['hcom'][i] + "\" HPc=\"" + Main_datframe['hpc'][i]
-                            + "\" HBmin=\"" + Main_datframe['hbpc'][i] + "\" HBPc=\"0\" HKnDnPc=\"" +  Main_datframe['knock_down'][i] + "\" />\n")
-                    
-                        out.write("      </Component>\n")
-                    out.write("    </Rotation_Components>\n")
-
-                    out.write("  </Experiment>\n")
-            out.write("</XDB>\n")
+        
+        
     
 
 
---
+++ misc_func.py
@@ -0,0 +1,88 @@
+
+
+####
+# misc_func.py
+
+#Author: Prateek
+#Note: This code is directly taken from Lydia's version
+#Taken from: 
+# Lydia Rill
+# 
+# Python 3.6
+#
+# This script contains a python function used in creating xdbfile
+#
+####
+
+import os
+import sys
+from datetime import datetime, timedelta
+import math
+import xml.etree.ElementTree as ET
+import pandas as pd
+import numpy as np
+
+# Check if a file or directory exists.
+# If it does not exist, exit the script!
+# You must set fileflag=True for a file.
+def check_exists_error(tmp_location, **kwargs):
+    fileflag = False
+    if 'fileflag' in kwargs:
+        fileflag = kwargs['fileflag']
+    if fileflag:
+        if not os.path.isfile(tmp_location):  # Check if the file exists.
+            st.write("## ERROR: cannot find the file", tmp_location)
+            #sys.exit()
+    else:
+        if not os.path.isdir(tmp_location):  # Check if the directory exists.
+            st.write("ERROR: cannot find the directory", tmp_location)
+            #sys.exit()
+
+# Calculate the average Bulk Density for 2 layers (top and bottom with defined threshold).
+def average_bd(soil_id, root, thresh_depth):
+    soil_layer = root.find('Soils/Soil[@SoilID="' + soil_id + '"]')
+    if soil_layer is None:
+        print("Error: soil layer does not exist in root", soil_id)
+        return(np.nan, np.nan, np.nan, np.nan)
+    else:
+        # Loop through each layer in the soil.
+        bd_dict = {}
+        top_bd_list = []
+        bottom_bd_list = []
+        for lyr in soil_layer.findall('Layer'):
+            depth = float(lyr.attrib['ZLYR'])  # Depth in cm.
+            bd = float(lyr.attrib['BD'])  # Bulk density.
+            bd_dict[depth] = bd
+            if depth <= thresh_depth:
+                top_bd_list.append(bd)  # Add the bulk density value to the top layer list.
+            else:
+                bottom_bd_list.append(bd)  # Add the bulk density value to the bottom layer list.
+
+        # Determine the min and max depth of the soil.
+        min_depth = min(bd_dict.keys())
+        max_depth = max(bd_dict.keys())
+
+        # Calculate the average bulk density using layers above the provided threshold.
+        if top_bd_list:
+            avg_bd_top = sum(top_bd_list) / len(top_bd_list)
+        else:
+            # If no layer began before 30 cm, use the BD of the first layer.
+            avg_bd_top = bd_dict[min_depth]
+        # Calculate the average bulk density using layers below the provided threshold.
+        if bottom_bd_list:
+            avg_bd_bottom = sum(bottom_bd_list) / len(bottom_bd_list)
+        else:
+            # No layers deeper than 30cm.
+            avg_bd_bottom = None
+
+        # Check to make sure the means values are ok.
+        if avg_bd_top < 1 or avg_bd_top > 2:
+            print("WARNING: unreasonable bulk density for top layer", soil_id)
+        if avg_bd_bottom is not None:
+            if avg_bd_bottom < 1 or avg_bd_bottom > 2:
+                print("WARNING: unreasonable bulk density for bottom layer", soil_id)
+
+        # Calculate the top depth and bottom depth for the initial conditions, converting the threshold from cm to m.
+        top_depth = thresh_depth / 100.
+        bottom_depth = (max_depth - thresh_depth) / 100.
+        return (avg_bd_top, avg_bd_bottom, top_depth, bottom_depth, max_depth)
